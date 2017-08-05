4bfddc746092d6012eed1ca925773cf7c4cd5774
Corey Kress <coreykre@buffalo.edu>
2017-08-05 19:41:26 -0400

    incomplete file size checking

--- app.js
+++ app.js
@@ -10,21 +10,31 @@ app.get('/', function (req, res) {
 
 const photoMimeTypes = [
     'image/png',
-    'image/jpeg',
+    // 'image/jpeg',
     'image/gif',
 ];
 
 app.post('/new', function(req, res) {
-     var busboy = new Busboy({headers: req.headers});
+     var busboy = new Busboy({headers: req.headers, limits: { files: 1, fileSize: 50000}});
      var invalidFile = false;
 
      busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
        var saveTo = path.join('./appdata', filename);
 
+       file.on('limit', function() {
+           console.log('file too large');
+           invalidFile = true;
+           file.resume();
+           return;
+       });
+
        if (!photoMimeTypes.includes(mimetype)){
            console.log('wrong file type');
            invalidFile = true;
            file.resume();
+       }
+
+       if(invalidFile) {
            return;
        }
 
