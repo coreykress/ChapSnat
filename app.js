aeaea860879ca9137534ddf10b3f4d0b2710b9ed
Corey Kress <coreykre@buffalo.edu>
2017-08-05 19:18:10 -0400

    added simple mimeType validation

--- app.js
+++ app.js
@@ -3,20 +3,42 @@ const fs = require('fs');
 const path = require('path');
 const Busboy = require('busboy');
 const app = express();
-//app.use(busboy());
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+const photoMimeTypes = [
+    'image/png',
+    'image/jpeg',
+    'image/gif',
+];
+
 app.post('/new', function(req, res) {
      var busboy = new Busboy({headers: req.headers});
+     var invalidFile = false;
+
      busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
        var saveTo = path.join('./appdata', filename);
+
+       if (!photoMimeTypes.includes(mimetype)){
+           console.log('wrong file type');
+           invalidFile = true;
+           file.resume();
+           return;
+       }
+
        console.log('logged');
        file.pipe(fs.createWriteStream(saveTo));
      });
+
      busboy.on('finish', function(){
-       res.writeHead(200, {'Connection': 'close'});
+         console.log(invalidFile);
+         if (invalidFile) {
+             res.writeHead(400, {'Connection': 'close'});
+         } else {
+            res.writeHead(200, {'Connection': 'close'});
+         }
        res.end('complete');
      });
      return req.pipe(busboy);
