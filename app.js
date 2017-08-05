1120dd0098c46e09ab338cb1e05ca71a748307dc
josephpusztay <josephpu@buffalo.edu>
2017-08-05 01:06:27 -0400

    added busboy and awful form to handle user images

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const Busboy = require('busboy');
+const app = express();
+//app.use(busboy());
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/new', function(req, res) {
+     var busboy = new Busboy({headers: req.headers});
+     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+       var saveTo = path.join('.', filename);
+       console.log('logged');
+       file.pipe(fs.createWriteStream(saveTo));
+     });
+     busboy.on('finish', function(){
+       res.writeHead(200, {'Connection': 'close'});
+       res.end('complete');
+     });
+     return req.pipe(busboy);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
