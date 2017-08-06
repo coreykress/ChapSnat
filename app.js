b1e66f90e2f1f818ece3a4215c4e3b42ad8b68d4
Corey Kress <coreykre@buffalo.edu>
2017-08-06 15:01:22 -0400

    formatted code, made error handler more generic and created errors, rather than write read on response

--- app.js
+++ app.js
@@ -5,71 +5,69 @@ const Busboy = require('busboy');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 const photoMimeTypes = [
     'image/png',
-    // 'image/jpeg',
+    'image/jpeg',
     'image/gif',
 ];
 
-app.post('/new', function(req, res) {
-     var busboy = new Busboy({headers: req.headers, limits: { files: 1, fileSize: 50000}});
-     var invalidFile = false;
+app.post('/new', function (req, res, next) {
+    var busboy = new Busboy({headers: req.headers, limits: {files: 1, fileSize: 50000}});
+    var invalidFile = false;
+    var err = null;
 
-     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-       var saveTo = path.join('./appdata', filename);
+    busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+        var saveTo = path.join('./appdata', filename);
 
-       file.on('limit', function() {
-           console.log('file too large');
-           invalidFile = true;
-           file.resume();
-           return;
-       });
+        //file.on('limit', function() {
+        //    console.log('file too large');
+        //    err = new Error();
+        //    err.status = 400;
+        //    err.message = 'File Too Large';
+        //});
 
-       if (!photoMimeTypes.includes(mimetype)){
-           console.log('wrong file type');
-           invalidFile = true;
-           file.resume();
-       }
+        if (!photoMimeTypes.includes(mimetype)) {
+            console.log('wrong file type');
+            err = new Error();
+            err.status = 400;
+            err.message = 'Invalid File Type';
+        }
 
-       if(invalidFile) {
-           return;
-       }
+        if (err !== null) {
+            next(err);
+        }
 
-       console.log('logged');
-       file.pipe(fs.createWriteStream(saveTo));
-     });
+        console.log('logged');
+        file.pipe(fs.createWriteStream(saveTo));
+    });
 
-     busboy.on('finish', function(){
-         console.log(invalidFile);
-         if (invalidFile) {
-             res.writeHead(400, {'Connection': 'close'});
-         } else {
-            res.writeHead(200, {'Connection': 'close'});
-         }
-       res.end('complete');
-     });
-     return req.pipe(busboy);
+    busboy.on('finish', function () {
+        res.status(200);
+
+        res.end('complete');
+    });
+    return req.pipe(busboy);
 });
 
-app.get('*', function(req, res, next) {
+app.get('*', function (req, res, next) {
     var err = new Error();
     err.status = 404;
+    err.message = 'Page Not Found';
     next(err);
 });
 
-//404 handling
 app.use(function (err, req, res, next) {
-    if (err.status !== 404) {
-        next(err);
+    if (!err) {
+        next();
     }
 
-    res.status(404);
-    res.send('Page not found');
+    res.status(err.status);
+    res.json(err);
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
 });
